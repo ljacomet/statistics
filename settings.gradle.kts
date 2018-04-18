54a9e1575abb07ab487d2c80a5477af21ef69142
Louis Jacomet <ljacomet@gmail.com>
2018-04-18 08:41:39 +0200

    Update cache IP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,6 @@ rootProject.name = "statistics"
 
 buildCache {
     remote(HttpBuildCache::class.java) {
-        setUrl("http://192.168.0.29:8080/cache/")
+        setUrl("http://172.20.253.36:8080/cache/")
     }
 }
